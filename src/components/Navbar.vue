b587c8d9b3db2b1e47262c46641729efec93b2d5
vertocode <evertonvanoni1@gmail.com>
2023-02-20 18:38:22 -0300

    feat: improvement navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,12 @@
 <template>
-  <div class="content navbar bg-black text-white">
-    <router-link to="login">
+  <div class="content navbar">
+    <router-link to="login" @click="activeStep = 'login'" style="text-decoration: underline">
       <img title="login" src="https://cdn-icons-png.flaticon.com/512/9052/9052119.png" alt="login-icon">
     </router-link>
     <ul class="list">
-      <router-link to="product-list">
+      <router-link :to="step.link" v-for="step in steps" :style="{ 'text-decoration': activeStep === step.link ? 'underline': 'none' }" @click="activeStep = step.link">
         <navbar-item
-            item-name="Product list"
-        />
-      </router-link>
-      <router-link to="add-new-product">
-        <navbar-item
-            item-name="Add a new product (Development)"
+            :item-name="step.title"
         />
       </router-link>
     </ul>
@@ -24,10 +19,26 @@
 
 <script setup>
 import NavbarItem from './Navbar/NavbarItem.vue'
+import { ref } from "vue";
+
+let activeStep = ref('login')
+console.log(activeStep)
+const steps = [
+  {
+    link: 'product-list',
+    title: 'Product list'
+  },
+  {
+    link: 'add-new-product',
+    title: 'Add a new product (Development)'
+  },
+]
 </script>
 
 <style lang="scss">
 .content {
+  font-family: SF Pro Text,SF Pro Icons,Helvetica Neue,Helvetica,Arial,sans-serif;;
+  background-color: rgb(22,22,23);
   margin-bottom: 2rem;
   display: flex;
   align-items: center;
@@ -39,14 +50,25 @@ import NavbarItem from './Navbar/NavbarItem.vue'
       margin: 0 50px
     }
   }
+
   img {
     width: 70px;
     position: relative;
   }
+
   .badge {
     position: absolute;
     bottom: 5px;
     right: 35px;
+  }
+
+  a {
+    text-decoration: none;
+    font-size: 1.5em;
+  }
+
+  a:hover {
+    text-decoration: underline;
   }
 }
 </style>
--- src/components/Navbar/NavbarItem.vue
+++ src/components/Navbar/NavbarItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="navbar-item m-1">{{ itemName }}</li>
+  <li class="navbar-item m-1" style="text-decoration: none">{{ itemName }}</li>
 </template>
 
 <script setup>
@@ -10,8 +10,15 @@ const props = defineProps({
 
 <style lang="scss">
 .navbar-item {
+  color: rgba(255, 255, 255, .8);
   list-style-type: none;
   cursor: pointer;
   padding-left: 1rem;
+  li {
+    text-decoration: none;
+  }
+}
+.navbar-item:hover {
+  color: #05cadc;
 }
 </style>
