11faa22ca5e28f34522c9707a31b8d1546c891f4
vertocode <evertonvanoni1@gmail.com>
2023-05-17 19:29:52 -0300

    feat: add a pagination

--- src/Pages/ProductList.vue
+++ src/Pages/ProductList.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="product-list">
-    <loading v-if="state.isLoading" :is-loading="true"></loading>
-    <div v-else>
-      <h1 class="mt-3">All products: </h1>
-      <multi-filter
-          :set-is-loading="setIsLoading"
-          :filters="filters"
-      ></multi-filter>
-      <div class="all-products" v-if="store.state.products.length">
-        <div class="product-item" v-for="(product, index) in store.state.products" :key="index">
-          <product-card
-              :index-product="index"
-              :value="product.value"
-              :name="product.name"
-              :description="product.description"
-              :src-img="product.srcImg"
-              :specifications="product.specifications"
-          />
+      <main>
+        <loading v-if="state.isLoading" :is-loading="true"></loading>
+        <div v-else>
+            <h1 class="mt-3">All products: </h1>
+            <multi-filter
+                    :set-is-loading="setIsLoading"
+                    :filters="filters"
+            ></multi-filter>
+            <div class="all-products" v-if="store.state.products.length">
+                <div class="product-item" v-for="(product, index) in store.state.products" :key="index">
+                    <product-card
+                            :index-product="index"
+                            :value="product.value"
+                            :name="product.name"
+                            :description="product.description"
+                            :src-img="product.srcImg"
+                            :specifications="product.specifications"
+                    />
+                </div>
+            </div>
+            <div v-else>
+                <none-products></none-products>
+            </div>
         </div>
-      </div>
-      <div v-else>
-        <none-products></none-products>
-      </div>
-    </div>
+      </main>
+      <footer class="m-auto">
+          <pagination />
+      </footer>
   </div>
   <add-new-product v-if="store.state.isAdmin"/>
 </template>
@@ -33,6 +38,7 @@ import ProductCard from '../components/ProductList/ProductCard.vue'
 import NoneProducts from '../components/ProductList/NoneProducts.vue'
 import MultiFilter from '../components/Input/MultiFilter.vue'
 import Loading from '../components/Loading/Loading.vue'
+import Pagination from '../components/ProductList/Pagination.vue'
 
 import { useStore } from 'vuex'
 import { Products } from "../services/product/usecases/product-list";
---
+++ src/components/ProductList/Pagination.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="pagination" aria-label="Page navigation example">
+        <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+    </nav>
+</template>
+
+<style lang="scss">
+.pagination {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
